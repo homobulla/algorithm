2186df03003210058e37faa34606e526d40f59ef
homobulla <homobulla@outlook.com>
2018-09-13 14:56:01 +0800

    feat: add remove-duplicates-from-sorted-array

---
+++ 26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,82 @@
+/*
+ * [26] Remove Duplicates from Sorted Array
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/description/
+ *
+ * algorithms
+ * Easy (39.41%)
+ * Total Accepted:    45.3K
+ * Total Submissions: 114.9K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * 
+ * 示例 1:
+ * 
+ * 给定数组 nums = [1,1,2], 
+ * 
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
+ * 
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 
+ * 示例 2:
+ * 
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ * 
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ * 
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 
+ * 
+ * 说明:
+ * 
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * 
+ * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ * 
+ * 你可以想象内部操作如下:
+ * 
+ * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+ * int len = removeDuplicates(nums);
+ * 
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素
+// 这句话是说，并非直接将原始数组改成符合条件的数组，而是将不重复的每一项扔到最前面
+// var removeDuplicates = function(nums) {
+//     // return (nums = [...new Set(nums)])
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i - 1] === nums[i] || nums[i + 1] === nums[i]) {
+//             nums.splice(i, 1)
+//             i -= 1
+//         }
+//     }
+
+//     return nums.length
+// }
+
+var removeDuplicates = function(nums) {
+    var j = 0
+    nums.map((v, i) => {
+        if (nums[i] != nums[i - 1]) {
+            nums[j++] = nums[i]
+        }
+    })
+    return j
+}
+// var a = [1]
+// console.log(removeDuplicates(a))
+// console.log(a)
